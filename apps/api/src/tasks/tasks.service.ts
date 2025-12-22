40e59e268da383352367f92de36364c03879868b
Leonardo Vitale <leonardo.vitale@outlook.com>
2025-12-22 13:14:03 -0300

    chore: fix apollo express5 adapter

--- apps/api/src/tasks/tasks.service.ts
+++ apps/api/src/tasks/tasks.service.ts
@@ -5,6 +5,15 @@ import { Task } from './task.model';
 export class TasksService {
   private tasks: Task[] = [
     { id: '1', title: 'Initial Task (Default Arch)', completed: false },
+    { id: '2', title: 'Initial Task (Default Arch)', completed: false },
+    { id: '3', title: 'Initial Task (Default Arch)', completed: false },
+    { id: '4', title: 'Initial Task (Default Arch)', completed: false },
+    { id: '5', title: 'Initial Task (Default Arch)', completed: false },
+    { id: '6', title: 'Initial Task (Default Arch)', completed: false },
+    { id: '7', title: 'Initial Task (Default Arch)', completed: false },
+    { id: '8', title: 'Initial Task (Default Arch)', completed: false },
+    { id: '9', title: 'Initial Task (Default Arch)', completed: false },
+    { id: '10', title: 'Initial Task (Default Arch)', completed: false },
   ];
 
   findAll(): Task[] {
@@ -21,4 +30,3 @@ export class TasksService {
     return newTask;
   }
 }
-
