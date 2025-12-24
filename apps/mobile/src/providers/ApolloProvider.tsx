24d48fb2404a23d42f79378e3dea7e60f88b9dbc
Leonardo Vitale <leonardo.vitale@outlook.com>
2025-12-24 01:31:02 -0300

    refactor(mobile): use centralized API config in ApolloProvider

--- apps/mobile/src/providers/ApolloProvider.tsx
+++ apps/mobile/src/providers/ApolloProvider.tsx
@@ -1,14 +1,11 @@
 import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client';
 import { ApolloProvider as BaseApolloProvider } from '@apollo/client/react';
 import { ReactNode } from 'react';
-
-// Use your machine's IP address for development (localhost won't work on device)
-// Replace with your actual IP for physical devices
-const API_URL = process.env.EXPO_PUBLIC_API_URL ?? 'http://192.168.15.7:4000/graphql'
+import { MOBILE_API_CONFIG } from '@repo/config/env';
 
 const client = new ApolloClient({
   link: new HttpLink({
-    uri: API_URL,
+    uri: MOBILE_API_CONFIG.graphqlEndpoint,
   }),
   cache: new InMemoryCache(),
 });
