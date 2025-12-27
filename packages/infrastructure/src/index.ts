0f1e6f4dac87276bc1cc2da326958d1ad07aff07
Leonardo Vitale <leonardo.vitale@outlook.com>
2025-12-27 17:25:06 -0300

    refactor(infrastructure): move files to feature structure
    - Move repositories to features/tasks/repositories/
    - Move mappers to features/tasks/mappers/
    - Move hooks to features/tasks/hooks/
    - Update imports to new domain/application structure

--- packages/infrastructure/src/index.ts
+++ packages/infrastructure/src/index.ts
@@ -1,13 +1,7 @@
-// Mappers
-export * from './mappers';
-
-// Repositories
-export * from './repositories/apollo-task-repository';
-
-// Query Hooks
-export * from './hooks/use-apollo-tasks-query';
-
-// Mutation Hooks
-export * from './hooks/use-apollo-create-task';
-export * from './hooks/use-apollo-toggle-task';
-export * from './hooks/use-apollo-delete-task';
+// Features
+export * from './features/tasks/mappers';
+export * from './features/tasks/repositories/apollo-task-repository';
+export * from './features/tasks/hooks/use-apollo-tasks-query';
+export * from './features/tasks/hooks/use-apollo-create-task';
+export * from './features/tasks/hooks/use-apollo-toggle-task';
+export * from './features/tasks/hooks/use-apollo-delete-task';
